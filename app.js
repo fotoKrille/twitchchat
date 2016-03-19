e857de1eadbe057a7356fb5b04b91fe8d39758c4
Krister Johansson <info@fotokrille.se>
2016-03-19 17:40:52 +0100

    chat

---
+++ app.js
@@ -0,0 +1,80 @@
+﻿var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var irc = require("tmi.js");
+var http = require('http');
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+server.listen(process.env.PORT);
+
+var options = {
+    identity: {
+        username: process.env.username,
+        password: process.env.password
+    },
+    channels: [rocess.env.channels]
+};
+
+var client = new irc.client(options);
+
+// Connect the client to the server..
+client.connect();
+
+io.on('connection', function (socket) {
+    client.on('chat', function (channel, user, message, self) {
+        socket.emit('chat', { user: user, message: message });
+    });
+});
+
